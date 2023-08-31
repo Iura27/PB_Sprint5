719b4fdc023b77caf4c8036943684ca914c20b0c
Iura Kurtz Filho <iura.filho@gmail.com>
2023-08-31 20:01:19 -0300

    Dia 1

---
+++ CapybaraCucumber/features/pages/google_home.rb
@@ -0,0 +1,10 @@
+
+
+class GoogleHome < BasePage
+
+    def search_for(query)
+        find("textarea[name='q']").set query
+        click_button 'Pesquisa Google'
+    end
+
+end
---
+++ CapybaraCucumber/features/pages/google_results.rb
@@ -0,0 +1,5 @@
+
+class GoogleResults < BasePage
+   
+
+end
